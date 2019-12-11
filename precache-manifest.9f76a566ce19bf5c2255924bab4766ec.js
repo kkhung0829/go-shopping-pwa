4c2e599bff8cd22655909a73eb10661d24870b90
KK Hung <kkhung@astri.org>
2019-12-11 15:44:00 +0800

    Updates

---
+++ precache-manifest.9f76a566ce19bf5c2255924bab4766ec.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11bc5b9213585718eaeb0205ce1d16bf",
+    "url": "/go-shopping-pwa/index.html"
+  },
+  {
+    "revision": "ed4fe0594a43fb3be75b",
+    "url": "/go-shopping-pwa/static/css/2.ea7d3e80.chunk.css"
+  },
+  {
+    "revision": "e70a2c191c11e8bdb7c1",
+    "url": "/go-shopping-pwa/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ed4fe0594a43fb3be75b",
+    "url": "/go-shopping-pwa/static/js/2.47587abe.chunk.js"
+  },
+  {
+    "revision": "f8513fb9f2981a1b206fb733b985f272",
+    "url": "/go-shopping-pwa/static/js/2.47587abe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e70a2c191c11e8bdb7c1",
+    "url": "/go-shopping-pwa/static/js/main.e126843d.chunk.js"
+  },
+  {
+    "revision": "760af712d51f6a31b1db",
+    "url": "/go-shopping-pwa/static/js/runtime-main.4740591d.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/go-shopping-pwa/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/go-shopping-pwa/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/go-shopping-pwa/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/go-shopping-pwa/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/go-shopping-pwa/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/go-shopping-pwa/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/go-shopping-pwa/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/go-shopping-pwa/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/go-shopping-pwa/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/go-shopping-pwa/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/go-shopping-pwa/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/go-shopping-pwa/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/go-shopping-pwa/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/go-shopping-pwa/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/go-shopping-pwa/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/go-shopping-pwa/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
